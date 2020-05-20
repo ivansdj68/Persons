4816f03527488538652a2e12c609dcb87d469e61
Ivan Santiago De Jesus <santiago_85519@students.pupr.edu>
2020-05-20 18:58:19 -0400

    Added authors in main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# Authors:
+#
+# Name:                     Student ID:
+# Amanda Burgos De Jesus    #97280
+# Hiram Zengotita Hernandez #103450
+# Ivan Santiago De Jesus    #85519
+#
+
 from AddressBook import *
 from AddressBookWindow import *
 from tkinter import *
